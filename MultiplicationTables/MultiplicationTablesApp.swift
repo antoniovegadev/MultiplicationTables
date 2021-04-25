4e7b1960ba5e710f1df2abbed8cae29949a8af70
antonivegaochoa <tonivegaochoa@gmail.com>
2021-04-25 09:47:11 -0400

    Initial Commit

---
+++ MultiplicationTables/MultiplicationTablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationTablesApp.swift
+//  MultiplicationTables
+//
+//  Created by Antonio Vega on 4/25/21.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationTablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
